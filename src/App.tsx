423f47132311bac4795e8c24562d89bff40eae52
jpg723 <86431761+jpg723@users.noreply.github.com>
2023-10-31 14:48:18 +0900

    [DELETE] 프로필 수정 화면 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import FriendSet from './Page/FriendSet';
 import ChatGPT from './Page/ChatGPT';
 import { GlobalStyle } from './GlobalStyle';
 import StartPage from './Page/StartPage';
-import ProfilePage from './Page/ProfilePage';
 import MyPage from './Page/MyPage';
 import OAuth2RedirectHandeler from './Page/OAuth2RedirectHandeler';
 
@@ -24,7 +23,6 @@ function App() {
           <Route element={<Header />}>
             <Route path="/bag-list" element={<MainBagPage />} /> {/*내 가방 리스트(추가)*/}
             <Route path="/mypage" element={<MyPage />} /> {/*프로필 수정*/}
-            <Route path="/ProfilePage" element={<ProfilePage />} /> {/*시작 프로필 설정*/}
           </Route>
         </Routes>
         <Routes>
