76109627938670a6948727eb3fd31001f054f104
jung <wldud2550@naver.com>
2023-11-07 17:35:44 +0900

    [ADD] openBag

--- src/Components/Mainpage/Bag_state_modal.tsx
+++ src/Components/Mainpage/Bag_state_modal.tsx
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from 'react';
-import styled, {createGlobalStyle} from "styled-components";
+import React, { useState, useEffect } from 'react';
+import styled, { createGlobalStyle } from "styled-components";
 import { ReactComponent as Bag_add_arrow } from '../../svg/bag_add_arrow.svg';
 import axios from 'axios';
 import { useNavigate, useParams } from 'react-router-dom';
@@ -93,29 +93,33 @@ function Bag_state_modal() {
 
   //닫기 버튼 클릭 시
   const [isbagState, setIsbagState] = useRecoilState(bagState);
+  const [status, setStatus] = useState('');
 
-   const openModalHandler = () => {
+  const openModalHandler = () => {
     setIsbagState(false);
   };
 
   const bag_id = useParams().bagId;
 
-  useEffect(()=> {
+  useEffect(() => {
     axios({
       url: `/bag/${bag_id}`,
       method: 'GET'
     }).then((response) => {
       console.log(response.data);
+      setStatus(response.data.status);
     }).catch((error) => {
       console.error('AxiosError:', error);
     });
-  },[])  
+  }, [])
 
   //완료 버튼 클릭시
   const navi = useNavigate();
-  function onClick_Bagstate(){
+  const apiUrl = status === "AVAILABLE" ? `/bag/close/${Number(bag_id)}` : `/bag/open/${Number(bag_id)}`;
+
+  function onClick_Bagstate() {
     axios({
-      url: `/bag/close/${Number(bag_id)}`,
+      url: apiUrl,
       method: 'PUT',
     }).then((response) => {
       console.log(response.data);
@@ -126,23 +130,30 @@ function Bag_state_modal() {
     setIsbagState(false);
   }
 
-
   return (
-      <ModalContainer>
-        <GlobalStyle/>
-        <ModalView>
-            <Bag_add_modal_header>
-                <Bag_add_modal_header_text>가방 상태 변경</Bag_add_modal_header_text>
-            </Bag_add_modal_header>
-            <Bag_add_modal_main>
-              짐 싸기가 완료되었나요?
-              <Btn_container>
-                <Bag_del_btn onClick={onClick_Bagstate}>완료</Bag_del_btn>
-                <Bag_del_btn onClick={openModalHandler}>취소</Bag_del_btn>
-              </Btn_container>
-            </Bag_add_modal_main>
-        </ModalView>
-      </ModalContainer>
+    <ModalContainer>
+      <GlobalStyle />
+      <ModalView>
+        <Bag_add_modal_header>
+          <Bag_add_modal_header_text>가방 상태 변경</Bag_add_modal_header_text>
+        </Bag_add_modal_header>
+        <Bag_add_modal_main>
+          {status === 'AVAILABLE' ? (
+            '짐 싸기가 완료되었나요?'
+          ) : (
+            '짐 싸기를 다시 시작하시나요?'
+          )}
+          <Btn_container>
+            <Bag_del_btn onClick={onClick_Bagstate}> {status === 'AVAILABLE' ? (
+              '완료'
+            ) : (
+              '복귀'
+            )}</Bag_del_btn>
+            <Bag_del_btn onClick={openModalHandler}>취소</Bag_del_btn>
+          </Btn_container>
+        </Bag_add_modal_main>
+      </ModalView>
+    </ModalContainer>
   );
 }
 
